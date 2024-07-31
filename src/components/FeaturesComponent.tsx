a83d7a3b2ca1cca0f93c871df57cd20918b19468
Percival Aceron <perci.aceron@gmail.com>
2024-07-31 11:58:02 +0800

    testing

--- src/components/FeaturesComponent.tsx
+++ src/components/FeaturesComponent.tsx
@@ -27,39 +27,41 @@ interface CardProps {
   href: string
 }
 
-const Card = ({ heading, description, icon, href }: CardProps) => {
+const Card = ({ heading, description, icon }: CardProps) => {
   return (
     <Box
-      maxW={{ base: 'full', md: '275px' }}
-      w={'full'}
+      maxW={{ base: "full", md: "275px" }}
+      w={"full"}
       borderWidth="1px"
       borderRadius="lg"
       overflow="hidden"
-      p={5}>
-      <Stack align={'start'} spacing={2}>
+      p={5}
+    >
+      <Stack align={"start"} spacing={2}>
         <Flex
           w={16}
           h={16}
-          align={'center'}
-          justify={'center'}
-          color={'white'}
-          rounded={'full'}
-          bg={useColorModeValue('gray.100', 'gray.700')}>
+          align={"center"}
+          justify={"center"}
+          color={"white"}
+          rounded={"full"}
+          bg={useColorModeValue("gray.100", "gray.700")}
+        >
           {icon}
         </Flex>
         <Box mt={2}>
           <Heading size="md">{heading}</Heading>
-          <Text mt={1} fontSize={'sm'}>
+          <Text mt={1} fontSize={"sm"}>
             {description}
           </Text>
         </Box>
-        <Button variant={'link'} colorScheme={'blue'} size={'sm'}>
+        <Button variant={"link"} colorScheme={"blue"} size={"sm"}>
           Learn more
         </Button>
       </Stack>
     </Box>
-  )
-}
+  );
+};
 
 export default function GridListWith() {
   return (
